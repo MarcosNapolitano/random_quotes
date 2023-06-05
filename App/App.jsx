be339c74b037592160640e1880868558aeb887c4
unknown <marcosnapolitano@hotmail.com.ar>
2023-06-05 10:02:57 -0300

    first Commit

---
+++ App/App.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { quotes, authors, colors } from './data.js'
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      quotes: quotes,
+      authors: authors,
+      colors: colors,
+      color: "rgb(143, 35, 35)",
+      quote:'',
+      author:''
+    };
+    this.newQuote = this.newQuote.bind(this);
+  };
+  
+  componentDidMount() {
+    
+    window.addEventListener('load', this.newQuote) 
+    this.newQuote() 
+    
+  }
+
+  componentWillUnmount() { 
+    window.removeEventListener('load', this.newQuote)  
+  }
+  
+  newQuote() {
+    const Index = Math.floor(Math.random() * this.state.quotes.length);
+    const Color = Math.floor(Math.random() * this.state.colors.length);
+
+    $('#text').fadeToggle(750)
+    $('#author').fadeToggle(750)
+    
+    setTimeout(()=>{
+
+      this.setState({
+        quote: this.state.quotes[Index],
+        author: this.state.authors[Index],
+        color: this.state.colors[Color]
+      });
+
+      $("Body").animate({backgroundColor: this.state.color, color: this.state.color}, 750)
+      $("#twitter").animate({color: this.state.color}, 750)
+      $("#new-quote").animate({backgroundColor: this.state.color}, 750)
+      $('#text').fadeToggle(750)
+      $('#author').fadeToggle(750)
+
+    },750)
+    
+
+  };
+
+
+  render() {
+    return(
+      <div id='quote-box' onLoad={this.newQuote}>
+        <div id='quote'>
+          <h2 id='text'>{this.state.quote}</h2>
+        </div>
+        <p id='author'>- {this.state.author}</p>
+        <div id="link-container">
+          <a href={`https://twitter.com/intent/tweet?text="${this.state.quote}"- ${this.state.author}`} target="_blank" id="tweet-quote">
+            <i className="fa-brands fa-twitter-square" id="twitter" />
+          </a>
+          <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+        </div>
+      </div>
+    )
+  }
+};
+  
+  
+export default App
---
+++ App/data.js
@@ -0,0 +1,40 @@
+const quotes = [
+    "We cannot solve problems with the kind of thinking we employed when we came up with them.", 
+    "Learn as if you will live forever, live like you will die tomorrow.", 
+    "Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.",
+    "When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.", 
+    "When you change your thoughts, remember to also change your world.",
+    "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.", 
+    "Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.",
+    "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    "It is better to fail in originality than to succeed in imitation.",
+    "The road to success and the road to failure are almost exactly the same.", 
+    "Success usually comes to those who are too busy looking for it.", 
+    "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.", 
+    "Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. The slogan 'Press On' has solved and always will solve the problems of the human race.", 
+    "There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.", 
+    "Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable.", 
+    "I never dreamed about success. I worked for it.",
+    "Success is getting what you want, happiness is wanting what you get."
+]
+
+const authors = [
+    "Albert Einstein", "Mahatma Gandhi", "Mark Twain", "Eleanor Roosevelt", 
+    "Norman Vincent Peale", "Walter Anderson", "Diane McLaren", "Winston S. Churchill", 
+    "Herman Melville", "Colin R. Davis", "Henry David Thoreau", "Dale Carnegie", 
+    "Calvin Coolidge", "Mister Rogers", "John Wooden", "Estée Lauder", "W. P. Kinsella"
+]
+
+const colors = [
+    "rgb(143, 35, 35)",
+    "rgb(143, 84, 35)",
+    "rgb(130, 143, 35)",
+    "rgb(35, 143, 48)",
+    "rgb(35, 143, 121)",
+    "rgb(35, 103, 143)",
+    "rgb(116, 35, 143)",
+    "rgb(143, 35, 109)"
+]
+
+export {quotes, authors, colors}
+
